59f26956b1f3296e2ebbdc1bd81460c17d1c1512
nokiroki <nikita@nokiroki.ru>
2023-03-10 12:31:44 +0300

    fixes

--- test.py
+++ test.py
@@ -3,14 +3,14 @@ import pickle
 
 from tqdm import tqdm
 
-from api import WbApi
+from wbapi.api import WbApi
 
 cards_to_modify = []
 for file_name in os.listdir('data/cards_empty_dop_1/'):
     with open(f'data/cards_empty_dop_1/{file_name}', 'rb') as f:
         cards_to_modify.extend(list(pickle.load(f)))
 
-with WbApi.create_wb_api('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6IjA0MGEwYWQ0LWZiOGEtNGIwYS04NTM0LTQ5ZDUyYWJiOTlkYSJ9.rUoHKDBTQARX2t-M5gX87M5zY9kDBuJaFJmJfxfCtoA') as wb_api:
+with WbApi.create_wb_api() as wb_api:
     for i in tqdm(range(100000, len(cards_to_modify), 100)):
         wb_api.change_cards(cards_to_modify[i:i + 100])
 
